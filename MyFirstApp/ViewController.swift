d7226ebe4e123a3e1c6f35fd0da0f9e910544c37
İsmail Arslan <>
2020-08-06 17:54:36 +0300

    third commit

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("test1")
+        print("test2")
     }
 
 
