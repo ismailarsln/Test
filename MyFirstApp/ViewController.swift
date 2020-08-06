03cf7149bdf8c12e1daef68b433a0566b466efc6
İsmail Arslan <>
2020-08-06 17:28:31 +0300

    first commit

---
+++ MyFirstApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MyFirstApp
+//
+//  Created by İsmail Arslan on 5.08.2020.
+//  Copyright © 2020 İsmail Arslan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+
+}
+
